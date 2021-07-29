02cc5270d2a0b719f7b653efb294a21e02283e64
Di Guan <dguan@purdue.edu>
2021-07-29 11:27:51 +0800

    test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,8 +1,9 @@
-package PACKAGE_NAME;/**
- * 
- *
+/**
  * @author Di Guan on 7/29/2021 11:25 AM
  */
 
 public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
